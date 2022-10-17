48c3e468ef831e8694ab7c284522daccd04212e8
alisha60 <115443520+alisha60@users.noreply.github.com>
2022-10-17 15:20:33 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int val;
+    printf("Enter the Value: ");
+    scanf("%d", &val);
+    printf("You entered %d", val);
+    getch();
+    return 0;
+}
